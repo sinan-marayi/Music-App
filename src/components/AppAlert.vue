e587588bedbf52b058edd47cd15c605c0f3473b4
sinan-marayi <sinanmarayi8@gmail.com>
2023-07-25 02:28:08 +0530

    alert component added

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,20 @@
+<template >
+    <div class="text-white text-center mb-4 rounded font-bold py-3" :class="bgColor">
+        {{ message }}
+    </div>
+</template>
+<script>
+export default {
+    name:'AppAlert',
+    props:{
+        bgColor:{
+            type:String,
+            default:'bg-red-500'
+        },
+        message:{
+            type:String,
+            default:''
+        }
+    }
+}
+</script>
